c52073ee60d28f96a7e257b99d02d0a99352486d
Hojin Chang <hojinchang0202@gmail.com>
2025-04-03 19:56:59 -0700

    Colors changed (brighter)

--- ddr-hojin/Fallback Explosion.lua
+++ ddr-hojin/Fallback Explosion.lua
@@ -1,57 +1,108 @@
-local white = color("#c8c8c8")
-local yellow = color("#d3d341")
-local green = color("#41ff41")
-local blue = color("#6464ff")
-local purple = color("#a050ff")
-local lightblue = color("#32eeff")
+local COLOR_W1 = color("#b8eefe")
+local COLOR_W2 = color("#ffe564")
+local COLOR_W3 = color("#64ff75")
+local COLOR_W4 = color("#65cbff")
+local COLOR_W5 = color("#e765f7")
+local COLOR_SHOCK = color("#64c8ff")
+local COLOR_W1_R = 0.722
+local COLOR_W1_G = 0.933
+local COLOR_W1_B = 0.996
+
+
+
 
 local function invisible()
-	return function(self) self:blend(Blend.Add):diffuse(0,0,0,0) end
+	return 
+		function(self) 
+			self:
+			blend(Blend.Add):
+			diffuse(0,0,0,0) 
+		end
 end
 
 local function finish()
-	return function(self) self:finishtweening() end
+	return 
+		function(self) 
+			self:
+			finishtweening() 
+		end
 end
 
 local function shockbright()
-	return function(self)
-			self:blend(Blend.Add):diffuse(lightblue):diffusealpha(0.9):
-			zoom(2.5):sleep(0.2):linear(0.01):
+	return 
+		function(self)
+			self:
+			blend(Blend.Add):
+			diffuse(COLOR_SHOCK):
+			diffusealpha(0.9):
+			zoom(2.5):
+			sleep(0.2):
+			linear(0.01):
 			diffusealpha(0.0)
-	end
+		end
 end
 
 local function shockzap()
-	return function(self)
-			self:blend(Blend.Add):diffuse(white):diffusealpha(0.9):
-			rotationz(0):zoom(0.7):spring(0.05):
-			zoom(1.3):sleep(0.15):linear(0.01):
+	return 
+		function(self)
+			self:
+			blend(Blend.Add):
+			diffuse(COLOR_W1):
+			diffusealpha(0.9):
+			rotationz(0):
+			zoom(0.7):
+			spring(0.05):
+			zoom(1.3):
+			sleep(0.15):
+			linear(0.01):
 			diffusealpha(0.0)
-	end
+		end
 end
 
 local function ghostdim(c)
-	return function(self)
-		self:blend(Blend.Add):diffuse(c):diffusealpha(1.0):
-		zoom(0.9):linear(0.12):zoom(1):
-		diffusealpha(0.9):linear(0.05):diffusealpha(0)
-	end
+	return 
+		function(self)
+			self:
+			blend(Blend.Add):
+			diffuse(c):
+			diffusealpha(1.0):
+			zoom(0.9):
+			linear(0.12):
+			zoom(1):
+			diffusealpha(0.9):
+			linear(0.05):
+			diffusealpha(0)
+		end
 end
 
 local function ghostbright(c)
-	return function(self)
-		self:blend(Blend.Add):diffuse(c):diffusealpha(1.0):
-		zoom(0.64):linear(0.07):zoom(0.8):
-		diffusealpha(0.9):linear(0.02):diffusealpha(0)
-	end
+	return 
+		function(self)
+			self:
+			blend(Blend.Add):
+			diffuse(c):
+			diffusealpha(1.0):
+			zoom(0.64):
+			linear(0.07):
+			zoom(0.8):
+			diffusealpha(0.9):
+			linear(0.02):
+			diffusealpha(0)
+		end
 end
 
 local function holddiffuse()
-	return function(self)
-		self:sleep(0.12):blend(Blend.Add):diffuse(white):
-		diffuseshift():effectcolor1(1,1,1,1):effectcolor2(1,1,1,0.6):
-		effectperiod(0.11)
-	end
+	return 
+		function(self)
+			self:
+			sleep(0.11):
+			blend(Blend.Add):
+			diffuse(COLOR_W1):
+			diffuseshift():
+			effectcolor1(COLOR_W1_R,COLOR_W1_G,COLOR_W1_B,1.0):
+			effectcolor2(COLOR_W1_R,COLOR_W1_G,COLOR_W1_B,0.6):
+			effectperiod(0.08)
+		end
 end
 
 local t = Def.ActorFrame {
@@ -69,24 +120,24 @@ local t = Def.ActorFrame {
 		JudgmentCommand=finish(),
 		InitCommand=invisible(),
 		
-		W1Command=ghostdim(white),
-		W2Command=ghostdim(yellow),
-		W3Command=ghostdim(green),
-		W4Command=ghostdim(blue),
-		W5Command=ghostdim(purple),
-		HeldCommand=ghostdim(white),
+		W1Command=ghostdim(COLOR_W1),
+		W2Command=ghostdim(COLOR_W2),
+		W3Command=ghostdim(COLOR_W3),
+		W4Command=ghostdim(COLOR_W4),
+		W5Command=ghostdim(COLOR_W5),
+		HeldCommand=ghostdim(COLOR_W1),
 	},
 	NOTESKIN:LoadActor( Var "Button", "Tap Explosion Bright" ) .. {
 		JudgmentCommand=finish(),
 		InitCommand=invisible(),
 		
-		W1Command=ghostbright(white),
+		W1Command=ghostbright(COLOR_W1),
 	},
 	NOTESKIN:LoadActor( Var "Button", "Tap Explosion Mid" ) .. {
 		JudgmentCommand=finish(),
 		InitCommand=invisible(),
 		
-		W2Command=ghostbright(yellow),
+		W2Command=ghostbright(COLOR_W2),
 	},
 	NOTESKIN:LoadActor( Var "Button", "HitMine Explosion" ) .. {
 		InitCommand=invisible();
