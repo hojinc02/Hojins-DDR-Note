c0063c16de222a4eb9cc3f58f62c91b6c3f7a2d7
Hojin Chang <hojinchang0202@gmail.com>
2025-04-10 13:29:56 -0700

    huh

--- ddr-hojin/Down Hold Explosion.lua
+++
@@ -1,8 +0,0 @@
-local t = Def.ActorFrame {
-	Def.Sprite {
-	--Texture=NOTESKIN:GetPath( 'Down', 'Hold Explosion' );
-	Texture=NOTESKIN:GetPath( 'Down', 'Tap Explosion Dim' );
-	--Texture=NOTESKIN:GetPath( '_blank' );
-	};
-};
-return t;
